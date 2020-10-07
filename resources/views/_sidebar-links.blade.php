802b7d86532370942cddfb4b083d7f64df797db5
Nicholas Ciechanowski <nciechanowski@staff.ventraip.com>
2020-10-07 15:07:51 +1100

    Added Logout

--- resources/views/_sidebar-links.blade.php
+++ resources/views/_sidebar-links.blade.php
@@ -9,26 +9,11 @@
          >Explore</a></li>
     <li><a
         class="font-bold text-lg mb-4 block"
-        href="#"
-        >Notifications</a></li>
-    <li><a
-        class="font-bold text-lg mb-4 block"
-        href="#"
-        >Messages</a></li>
-    <li><a
-        class="font-bold text-lg mb-4 block"
-        href="#"
-        >Bookmarks</a></li>
-    <li><a
-        class="font-bold text-lg mb-4 block"
-        href="#"
-        >Lists</a></li>
-    <li><a
-        class="font-bold text-lg mb-4 block"
         href="{{ route('profile', auth()->user()) }}"
         >Profile</a></li>
-    <li><a
-        class="font-bold text-lg mb-4 block"
-        href="#"
-        >More</a></li>
-</ul>
+    <li>
+        <form action="/logout" method="post">
+            @csrf
+            <button class="font-bold text-lg block">Logout</button>
+        </form>
+    </ul>
